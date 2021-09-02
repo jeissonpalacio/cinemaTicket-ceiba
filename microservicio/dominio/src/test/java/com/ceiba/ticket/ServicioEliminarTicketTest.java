9fe1a8d89f626d7272636cce99889c091b08d81a
Jeisson Andrés Palacio Velásquez - Ceiba Software <jeisson.palacio@ceiba.com.co>
2021-09-02 09:40:55 -0500

    changes test

--- microservicio/dominio/src/test/java/com/ceiba/ticket/ServicioEliminarTicketTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/ticket/ServicioEliminarTicketTest.java
@@ -2,6 +2,7 @@ package com.ceiba.ticket;
 
 import com.ceiba.BasePrueba;
 import com.ceiba.movie_projector.puerto.repositorio.MovieProjectorRepositorio;
+import com.ceiba.seats.puerto.repositorio.RepositorioSeat;
 import com.ceiba.ticket.excepcion.ExcepcionExistenciaTicket;
 import com.ceiba.ticket.excepcion.ExcepcionTiempoDeCambio;
 import com.ceiba.ticket.puerto.repositorio.RepositorioTicket;
@@ -17,8 +18,9 @@ public class ServicioEliminarTicketTest {
     @Test
     public void validateTimeLimitTest(){
         RepositorioTicket repositorioTicket = Mockito.mock(RepositorioTicket.class);
+        RepositorioSeat repositorioSeat = Mockito.mock(RepositorioSeat.class);
         MovieProjectorRepositorio movieProjectorRepositorio = Mockito.mock(MovieProjectorRepositorio.class);
-        ServicioEliminarTicket servicioEliminarTicket = new ServicioEliminarTicket(repositorioTicket,movieProjectorRepositorio);
+        ServicioEliminarTicket servicioEliminarTicket = new ServicioEliminarTicket(repositorioTicket,movieProjectorRepositorio,repositorioSeat);
         LocalDate time = LocalDate.now();
         BasePrueba.assertThrows(()->servicioEliminarTicket.validateTimeLimit(time), ExcepcionTiempoDeCambio.class,"El tiempo de cambio paso");
     }
@@ -26,8 +28,9 @@ public class ServicioEliminarTicketTest {
     @Test
     public void validateExistTest(){
         RepositorioTicket repositorioTicket = Mockito.mock(RepositorioTicket.class);
+        RepositorioSeat repositorioSeat = Mockito.mock(RepositorioSeat.class);
         MovieProjectorRepositorio movieProjectorRepositorio = Mockito.mock(MovieProjectorRepositorio.class);
-        ServicioEliminarTicket servicioEliminarTicket = new ServicioEliminarTicket(repositorioTicket,movieProjectorRepositorio);
+        ServicioEliminarTicket servicioEliminarTicket = new ServicioEliminarTicket(repositorioTicket,movieProjectorRepositorio,repositorioSeat);
         Mockito.when(repositorioTicket.validarExiste(Mockito.anyLong())).thenReturn(false);
         BasePrueba.assertThrows(()->servicioEliminarTicket.validateExist(1L),ExcepcionExistenciaTicket.class,"No existe el ticket");
 
