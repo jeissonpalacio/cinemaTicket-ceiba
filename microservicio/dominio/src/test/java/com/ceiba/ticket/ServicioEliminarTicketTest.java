95b0c55242ba91ff19927e743b79e7d5ab199e1f
Jeisson Andrés Palacio Velásquez - Ceiba Software <jeisson.palacio@ceiba.com.co>
2021-09-03 14:43:04 -0500

    test delete ticket

--- microservicio/dominio/src/test/java/com/ceiba/ticket/ServicioEliminarTicketTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/ticket/ServicioEliminarTicketTest.java
@@ -8,6 +8,7 @@ import com.ceiba.ticket.excepcion.ExcepcionTiempoDeCambio;
 import com.ceiba.ticket.puerto.repositorio.RepositorioTicket;
 import com.ceiba.ticket.servicio.ServicioActualizarTicket;
 import com.ceiba.ticket.servicio.ServicioEliminarTicket;
+import com.ceiba.ticket.testdatabuilder.TicketTestDataBuilder;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -33,6 +34,29 @@ public class ServicioEliminarTicketTest {
         ServicioEliminarTicket servicioEliminarTicket = new ServicioEliminarTicket(repositorioTicket,movieProjectorRepositorio,repositorioSeat);
         Mockito.when(repositorioTicket.validarExiste(Mockito.anyLong())).thenReturn(false);
         BasePrueba.assertThrows(()->servicioEliminarTicket.validateExist(1L),ExcepcionExistenciaTicket.class,"No existe el ticket");
+
+    }
+    @Test
+    public void validateExistSuccesfulTest(){
+        RepositorioTicket repositorioTicket = Mockito.mock(RepositorioTicket.class);
+        RepositorioSeat repositorioSeat = Mockito.mock(RepositorioSeat.class);
+        MovieProjectorRepositorio movieProjectorRepositorio = Mockito.mock(MovieProjectorRepositorio.class);
+        ServicioEliminarTicket servicioEliminarTicket = new ServicioEliminarTicket(repositorioTicket,movieProjectorRepositorio,repositorioSeat);
+        Mockito.when(repositorioTicket.validarExiste(Mockito.anyLong())).thenReturn(true);
+        servicioEliminarTicket.validateExist(1L);
+        Mockito.verify(repositorioTicket,Mockito.atLeastOnce()).validarExiste(1L);
+    }
+
+    @Test
+    public void obtenerTicketTest(){
+        RepositorioTicket repositorioTicket = Mockito.mock(RepositorioTicket.class);
+        RepositorioSeat repositorioSeat = Mockito.mock(RepositorioSeat.class);
+        MovieProjectorRepositorio movieProjectorRepositorio = Mockito.mock(MovieProjectorRepositorio.class);
+        Mockito.when(repositorioTicket.obtenerTicket(1L)).thenReturn(new TicketTestDataBuilder().build());
+        Mockito.when(repositorioTicket.validarExiste(Mockito.anyLong())).thenReturn(true);
+        ServicioEliminarTicket servicioEliminarTicket = new ServicioEliminarTicket(repositorioTicket,movieProjectorRepositorio,repositorioSeat);
+        servicioEliminarTicket.obtenerTicket(1L);
+        Mockito.verify(repositorioTicket).obtenerTicket(1L);
 
     }
 
