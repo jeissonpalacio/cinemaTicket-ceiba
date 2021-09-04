ffb5221954cc27793c784863d0f9d28d7a07ecc6
Jeisson Andrés Palacio Velásquez - Ceiba Software <jeisson.palacio@ceiba.com.co>
2021-09-04 17:16:57 -0500

    correcion pruebsa

--- microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
+++ microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
@@ -1,7 +1,7 @@
 insert into usuario(nombre,clave,fecha_creacion) values('test','1234',now());
 insert into movie(name_movie,type_movie,lenght,rating) VALUES ('Joker','Comedy','1:40',4);
-insert into movie_projector(date_movie_projector,hour_movie_projector,id_movie,id_cinema) VALUES (TO_DATE('05-09-2021','dd-MM-yyyy'),PARSEDATETIME('12:30', 'HH:MM'),1,1);
-insert into movie_projector(date_movie_projector,hour_movie_projector,id_movie,id_cinema) VALUES (TO_DATE('29-09-2021','dd-MM-yyyy'),PARSEDATETIME('06:30', 'HH:MM'),1,1);
+insert into movie_projector(date_movie_projector,hour_movie_projector,id_movie,id_cinema) VALUES (TO_DATE('05-09-2022','dd-MM-yyyy'),PARSEDATETIME('12:30', 'HH:MM'),1,1);
+insert into movie_projector(date_movie_projector,hour_movie_projector,id_movie,id_cinema) VALUES (TO_DATE('29-09-2022','dd-MM-yyyy'),PARSEDATETIME('06:30', 'HH:MM'),1,1);
 insert into seats(number_seat,available,id_movie_projector) VALUES('A1',1,1);
 insert into seats(number_seat,available,id_movie_projector) VALUES('A2',1,1);
 insert into seats(number_seat,available,id_movie_projector) VALUES('A3',1,1);
